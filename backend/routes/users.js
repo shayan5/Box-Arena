c7ddfb584d375da8d089aa005f09d1408f2a15db
shayan5 <25065328+shayan5@users.noreply.github.com>
2020-06-07 23:37:51 -0400

    updated jwt authentication

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 let User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
@@ -9,6 +10,9 @@ router.route('/').get((req, res) => {
 router.route('/register').post((req, res) => {
     const username = req.body.username;
     const password = req.body.password;
+    if (!username || !password){
+        res.sendStatus(400).json('Error registering account');
+    }
     bcrypt.genSalt((err, salt) => {
         bcrypt.hash(password, salt, (err, hash) => {
             const newUser = new User({
@@ -20,6 +24,26 @@ router.route('/register').post((req, res) => {
                 .then(() => res.json('Registered Successfully'))
                 .catch(err => res.status(400).json(err));
         });
+    });
+});
+
+router.route('/login').post((req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    User.find({
+        username: username
+    }, (err, docs) => {
+        if (docs){
+            const user = new User(docs[0]);
+            bcrypt.compare(password, user.passwordHash, (err, same) => {
+                if (same){
+                    const accessToken = jwt.sign(username, process.env.ACCESS_TOKEN_SECRET);
+                    res.json({ accessToken : accessToken });
+                } else {
+                    res.status(401).json('Incorrect username or password');
+                }
+            });
+        }
     });
 });
 
