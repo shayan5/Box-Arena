b1e29377da3500cd9af529282f052409dd820583
S A <25065328+shayan5@users.noreply.github.com>
2020-06-22 22:03:10 -0400

    added disconnect button

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
 
-const client = new WebSocket("ws://www.test.com:4001");
+let client;
 
 class Game extends Component {
     constructor(props) {
         super(props);
 
+        this.connectToGame = this.connectToGame.bind(this);
+        this.disconnectFromGame = this.disconnectFromGame.bind(this);
+
         this.state = {
+            connect: false,
             message: ""
         }
     }
 
-    componentDidMount() {
+
+    componentWillUnmount() {
+        if (this.state.connect){
+            console.log("Disconnected")
+            client.close();
+        }
+    }
+
+    connectToGame() {
+        client = new WebSocket("ws://www.test.com:4001");
         client.onopen = () => {
             console.log("Connected");
         }
@@ -19,20 +32,29 @@ class Game extends Component {
         client.onmessage = (message) => {
             console.log(message);
         }
+        this.setState({ connect: true });
     }
 
-    componentWillUnmount() {
+    disconnectFromGame() {
         client.close();
+        console.log('Disconnected');
+        this.setState({ connect: false });
     }
 
     render() {
-        return(
-            <div>
-                <h1>Hello world</h1>
-                <p>{this.state.message}</p>
-            </div>
-        );
-        
+        if (this.state.connect){
+            return(
+                <div>
+                    <button onClick={this.disconnectFromGame}>Disconnect</button>
+                </div>
+            );
+        } else {
+            return(
+                <div>
+                    <button onClick={this.connectToGame}>Connect</button>
+                </div>
+            );
+        }       
     }
 }
 
